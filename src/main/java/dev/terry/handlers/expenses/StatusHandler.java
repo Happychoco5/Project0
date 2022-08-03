ea582643d30b99f7d0d5655a880c8f2361d85e3f
Happychoco5 <theendereye@yahoo.com>
2022-08-03 09:49:08 -0700

    Add other

---
+++ src/main/java/dev/terry/handlers/expenses/StatusHandler.java
@@ -0,0 +1,43 @@
+package dev.terry.handlers.expenses;
+
+import dev.terry.app.App;
+import dev.terry.entities.Expense;
+import dev.terry.entities.Status;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+
+public class StatusHandler implements Handler {
+    @Override
+    public void handle(@NotNull Context ctx) throws Exception {
+        int id = Integer.parseInt(ctx.pathParam("id"));
+
+        if(App.expenseService.getExpenseWithId(id) != null)
+        {
+            Expense expense = App.expenseService.getExpenseWithId(id);
+
+            //Execute code
+            if(ctx.path().toLowerCase().contains("approve"))
+            {
+                //We approve the expense.
+                SetStatus(expense, Status.APPROVED);
+                ctx.result("");
+            }
+            else if(ctx.path().toLowerCase().contains("deny")){
+                //We deny the expense
+                SetStatus(expense, Status.DENIED);
+                ctx.result("");
+            }
+        }
+        else {
+            //Return error.
+            ctx.status(404);
+            ctx.result("There is no ID of " + id + " in the expenses.");
+        }
+    }
+
+    public void SetStatus(Expense expense, Status status)
+    {
+        App.expenseService.updateStatus(expense, status);
+    }
+}
